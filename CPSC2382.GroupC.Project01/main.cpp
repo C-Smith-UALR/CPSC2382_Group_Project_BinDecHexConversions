a3423dc35f17bb62d110aa6695c48fcbb2aa4c17
C-Smith-UALR <36215531+C-Smith-UALR@users.noreply.github.com>
2018-09-09 20:25:58 -0500

    9_9_18 progress
    addition of Decimal to Binary conversion function

--- CPSC2382.GroupC.Project01/main.cpp
+++ CPSC2382.GroupC.Project01/main.cpp
@@ -3,9 +3,9 @@ CPSC 2382
 September 2018
 
 Group C:
-Caroline Azzone
+Caroline Azzone		FIXME:  enter T number
 Clark Smith			T00642044
-Tiger Nguyen
+Tiger Nguyen		FIXME:  enter T number
 
 
 A program that converts among various bases.  
@@ -16,10 +16,16 @@ A program that converts among various bases.
 
 using std::cout;
 using std::cin;
+using std::vector;
+using std::endl;
 
 int getUserInput();	
 void convertDecToBin();         //function declarations 
 void convertDecToHex();
+void convertBinToDec();
+void convertBinToHex();
+void convertHexToDec();
+void convertHexToBin();
 
 
 int main() {					// main function
@@ -38,7 +44,24 @@ int main() {					// main function
 		case DEC_HEX:
 			convertDecToHex();
 			break;
+		case BIN_DEC:
+			convertBinToDec();
+			break;
+		case BIN_HEX:
+			convertBinToHex();
+			break;
+		case HEX_DEC:
+			convertHexToDec();
+			break;
+		case HEX_BIN:
+			convertHexToBin();
+			break;
+		default:
+			cout << "Invalid selection\n";
+			break;
 		}
+		
+
 
 
 
@@ -57,7 +80,8 @@ int main() {					// main function
 
 
 int getUserInput() {
-	int baseSystem, targetSystem;
+	int baseSystem, targetSystem;			//this function is a series of nested switch statments that
+	system("CLS");							//obtains the desired systems from the user
 
 	do {
 		cout << "Enter the base number system: \n";
@@ -145,27 +169,37 @@ int getUserInput() {
 		return 6;
 
 	}
-
-
-
-
-
-
 	}
 
 }
 
 void convertDecToBin() {
-	int userValue, temp, i;
+	int userValue, temp, i, valueSave;
+	
 	
 	cout << "Enter the value in base: \n";
 	cin >> userValue;
 
-	std::vector<int>v; 
-	while (temp)
-	
+	valueSave = userValue;
 
+	std::vector<int>v;				//vector for storing the 1's and 0's of binary
+	while (userValue > 0) {
+		temp = userValue % 2;		//temp is either 1 or 0
+		v.push_back(temp);			//store temp in vector
+		userValue /= 2;
+	}
 
+	std::vector<int>::reverse_iterator it;   //reverse iterator so that the digits are printed in reverse
+
+	cout << valueSave << " in base 10 is equal to ";
+
+	for (it = v.rbegin(); it != v.rend(); ++it) {
+		cout << *it;							//dereferencing the iterator prints 1 or 0;
+	}
+
+	cout << " in base 2.\n\n\n";
+
+	system("PAUSE");
 
 }
 
@@ -173,5 +207,24 @@ void convertDecToBin() {
 
 void convertDecToHex() {
 
+							//FIXME:  code here for converting Decimal to Hex
+}
+
+void convertBinToDec() {
+
+						//FIXME:  code here for converting Binary to Decimal
+}
+
+void convertBinToHex() {
+						//FIXME:  code here for converting Binary to Hexadecimal
+
+}
+
+void convertHexToDec() {
+
+					//FIXME:  code here for converting Hexadecimal to Decimal
+}
+void convertHexToBin() {
+					//FIXME:  code here for converting Hexadecimal to Binary
 
 }
